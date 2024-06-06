d1f2e7400bea2417e22c7ea6286dbab905e719d1
Elsa <ehazzim@gmail.com>
2024-06-06 16:57:29 +0100

    Building + evaluation models

--- main.py
+++ main.py
@@ -63,3 +63,24 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=
 Training data now in X_train and Y_train for preparing models
 and X_validation and Y_validation sets to use later.
 """
+
+## BUILD AND EVALUATE MODELS
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+# Evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+ kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+ cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+ results.append(cv_results)
+ names.append(name)
+ print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
